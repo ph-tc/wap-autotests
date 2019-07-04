9200483fb7cfa3bde8ff2581a9f5544a537b29ec
ph-tc <49201522+ph-tc@users.noreply.github.com>
2019-07-04 16:16:41 +0300

    all checks finished, but need a refactor to disallow script stop running

--- autotest.beeline.js
+++ autotest.beeline.js
@@ -1,3 +1,53 @@
+// проверка наличия определённых ID и классов у тегов
+
+if ( document.getElementById('header-block') ) {
+	console.log(`присутствует элемент с id="header-block"`);
+} else {
+	console.error(`отсутствует элемент с id="header-block"`);
+}
+
+if ( document.querySelector('.header-text') ) {
+	console.log(`присутствует элемент с классом "header-text"`);
+} else {
+	console.error(`отсутствует элемент с классом "header-text"`);
+}
+
+if ( document.querySelector('.age-limit') ) {
+	console.log(`присутствует элемент с классом "age-limit"`);
+} else {
+	console.error(`отсутствует элемент с классом "age-limit"`);
+}
+
+if ( document.getElementById('main-block') ) {
+	console.log(`присутствует элемент с id="main-block"`);
+} else {
+	console.error(`отсутствует элемент с id="main-block"`);
+}
+
+if ( document.getElementById('info-block') ) {
+	console.log(`присутствует элемент с id="info-block"`);
+} else {
+	console.error(`отсутствует элемент с id="info-block"`);
+}
+
+if ( document.querySelector('.info-block-text') ) {
+	console.log(`присутствует элемент с классом "info-block-text"`);
+} else {
+	console.error(`отсутствует элемент с классом "info-block-text"`);
+}
+
+if ( document.getElementById('footer-block') ) {
+	console.log(`присутствует элемент с id="footer-block"`);
+} else {
+	console.error(`отсутствует элемент с id="footer-block"`);
+}
+
+if ( document.querySelector('.footer-text') ) {
+	console.log(`присутствует элемент с классом "footer-text"`);
+} else {
+	console.error(`отсутствует элемент с классом "footer-text"`);
+}
+
 // проверка шрифтов на странице (везде должен использоваться Open Sans)
 
 function detectFonts () {
@@ -33,22 +83,32 @@ function checkFonts( fontsArray ) {
 checkFonts( detectFonts() );
 
 
-// проверка закрытых одиночных тегов
-console.info(`Проверь закрытые одиночные теги\n<meta/> <link/> <br/> <img/> <input/> !`);
-
-
 // проверка кнопки
 const btn = document.getElementById('activation-btn');
-const btnFontSize = parseInt(getComputedStyle(btn.children[0]).fontSize);
+
+let btnFontSize;
+if (btn) {
+  btnFontSize = parseInt(getComputedStyle(btn.children[0]).fontSize); 
+} else {
+  console.error(`У кнопки отсутствует id='activation-btn'`);
+}
+
 const price = document.querySelector('.info-block-text .price .line2');
 
-(function checkButton(btn) {
-  if ( !btn ) {
-    console.error(`У кнопки отсутствует id='activation-btn'`);
-    return;
-  }
+let btnText;
+if (btn) {
+  btnText = btn.querySelector('.activation-btn-text'); 
+} else {
+  console.error(`У кнопки отсутствует id='activation-btn'`);
+}
 
-  if ( btn.tagName === 'BUTTON') {
+let btnTextStyles;
+if (btnText) {
+  btnTextStyles = getComputedStyle(btnText);
+}
+
+(function checkButton(btn) {
+  if ( btn && btn.tagName === 'BUTTON') {
     console.log(`Кнопка - ок ( button )`);
   } else {
     console.error(`Кнопка - не button!`);
@@ -101,6 +161,14 @@ const price = document.querySelector('.info-block-text .price .line2');
   } else {
     console.error(`Непрозрачный фон у цены отсутствует`);
   }
+
+	if ( !btnText ) {
+		console.error(`отсутствует элемент с классом "activation-btn-text"`);
+	} else if ( btnTextStyles.transform !== 'translate(-50%, -50%)' && btnTextStyles.position !== 'absolute' && 		btnTextStyles.top !== '50%' && btnTextStyles.left !== '50%' ) {
+		console.error(`текст на центральной кнопке не отцентрован`);
+	} else {
+		console.log(`текст на центральной кнопке отцентрован - ок`);
+	}
 })(btn);
 
 
@@ -119,7 +187,7 @@ const footerElems = document.querySelectorAll('#footer-block *');
 })()
 
 // напоминание base64
-console.info('Все картинки и шрифты в одном файле');
+console.info('Все картинки и шрифты в одном файле в base64');
 
 // проверка head
 
@@ -151,3 +219,119 @@ const headMeta4 = "<meta http-equiv=\"Content-Type\" content=\"text/html; charse
     console.log('<title> верный');
   }
 })()
+
+
+// проверка формы в теле body
+
+if ( document.body.firstElementChild === document.forms[0] ) {
+	console.log('Форма в body есть - ок!');
+} else {
+	console.error('Отсутствует форма в body!');
+}
+
+// проверка аттрибутов тега body
+
+if ( document.body.id !== 'resize' ) {
+	console.error(`ID у body дожен быть "resize"`);
+} else if ( !document.body.classList.contains('background') ) {
+	console.error(`class у body дожен содержать "background"`);
+} else {
+	console.log(`класс и id у body - ок`);
+}
+
+const attrs = document.body.attributes;
+
+for ( let i = 0; i < attrs.length; i++ ) {
+	if ( attrs[i].name !== 'th:if' && attrs[i].value !== '${isWebRequest} == false' && i === attrs.length - 1 ) {
+		console.error(`отсутствует макрос на теге body`);
+	} else if ( attrs[i].name !== 'th:if' && attrs[i].value !== '${isWebRequest} == false' ) {
+		continue
+	} else if ( attrs[i].name === 'th:if' && attrs[i].value === '${isWebRequest} == false' ) {
+		console.log(`макрос на теге body - ок`);
+		break;
+	}
+}
+
+// проверка наличия кнопок в LP
+
+const btns = document.querySelectorAll('button');
+
+if ( btns.length !== 1 ) {
+	console.error(`В lp только центральная кнопка должна быть BUTTON, остальные все - div`);
+} else {
+	console.log(`Одна кнопка на странице - ок`);
+}
+
+// проверка наличия скрипта после тега btnTextStyles
+
+const headStyleTag = document.querySelector('style');
+
+if ( headStyleTag.nextElementSibling.tagName === 'SCRIPT' &&
+		 headStyleTag.nextElementSibling.src === 'http://cdp.beeline.ru/scripts/script.js' ) {
+	console.log(`скрипт после тега style - ок`);
+} else {
+	console.error(`нету скрипта после тега style`);
+}
+
+// проверка наличия пользовательского соглашения
+
+if ( document.forms[0].nextElementSibling.id === 'overlay' ) {
+	console.log(`имеется пользовательское соглашение - ок`);
+} else {
+	console.error(`отсутсвует пользовательское соглашение`);
+}
+
+// проверка макросов в футере
+
+const footerSpans = document.querySelectorAll('#footer-block span');
+const footerTel = document.querySelector('#footer-block .tel');
+
+for (let i = 0; i < footerSpans.length; i++) {
+  let spanAttrs = footerSpans[i].attributes;
+  if ( spanAttrs.length === 0 ) {
+    console.error(`нет макроса в футере`);
+    break;
+  } else {
+    for (let k = 0; k < spanAttrs.length; k++) {
+      if ( k === spanAttrs.length - 1 && spanAttrs[k].name !== 'th:text' ) {
+        console.error(`нет макроса в футере`);
+      } else if ( spanAttrs[k].name !== 'th:text' && spanAttrs[k].value !== "${(#strings.arraySplit(partnerContact, ','))[0]}" || spanAttrs[k].value !== "${(#strings.arraySplit(partnerContact, ','))[2]}" ) {
+        continue
+      } else {
+        break;
+      }
+    }
+  }
+}
+
+const footerTelAttrs = footerTel.attributes;
+let telMacroses = 0;
+
+for (let i = 0; i < footerTelAttrs.length; i++) {
+  if ( i === footerTelAttrs.length - 1 && footerTelAttrs[i].name !== 'th:text' && footerTelAttrs[i].name !== 'th:href') {
+    console.error(`нет макроса в футере у телефона`);
+    break;
+  } else if ( footerTelAttrs[i].name === 'th:text' && footerTelAttrs[i].value === "${(#strings.arraySplit(partnerContact, ','))[1]}" || footerTelAttrs[i].name === 'th:href' &&footerTelAttrs[i].value === "${'tel:' + (#strings.arraySplit(partnerContact, ','))[1]}" ) {
+    telMacroses++;
+    continue;
+  } else {
+    continue;
+  }
+}
+
+if ( telMacroses == 2 ) {
+  console.log(`макросы в футере у телефона - ок`);
+} else {
+  console.error(`отсутствует макрос в футере у телефона`);
+}
+
+const footerOffer = document.querySelector('#footer-block a:last-child');
+
+if ( footerOffer.href === 'http://moskva.beeline.ru/customers/products/mobile/services/details/uslugi-na-korotkikh-nomerakh/' ) {
+  console.log(`ссылка на соглашение в футере - ок`);
+} else {
+  console.error(`неверная ссылка на соглашение в футере`);
+}
+
+// проверка закрытых одиночных тегов
+console.info(`Проверь закрытые одиночные теги\n<meta/> <link/> <br/> <img/> <input/> !`);
